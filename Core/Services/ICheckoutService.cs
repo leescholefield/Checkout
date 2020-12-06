05c442389674570813cb1206d51f3fffed22c5c2
Lee Scholefield <leescholefield@gmail.com>
2020-12-06 14:52:56 +0000

    Define interface for ICheckoutService. This will coordinate interaction between basket and pricingservice

---
+++ Core/Services/ICheckoutService.cs
@@ -0,0 +1,22 @@
+﻿using Core.Models;
+
+namespace Core.Services
+{
+    /// <summary>
+    /// This is reposinsible for keeping track of the items a user has added to their cart and returning the total price.
+    /// </summary>
+    public interface ICheckoutService
+    {
+        /// <summary>
+        /// Adds the specified number of items to the users basket.
+        /// </summary>
+        /// <param name="item">Item to add to the basket.</param>
+        /// <param name="count">Number of instances of item to add.</param>
+        public void Add(Item item, int count = 1);
+
+        /// <summary>
+        /// Returns the total price of all items in the basket.
+        /// </summary>
+        public decimal GetTotal();
+    }
+}
