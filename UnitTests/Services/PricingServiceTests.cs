bdc3abd17ec9e12b1af6d0bb03d672a944ddf814
Lee Scholefield <leescholefield@gmail.com>
2020-12-06 14:04:06 +0000

    Define test class for PricingService

---
+++ UnitTests/Services/PricingServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using Core.Models;
+using Core.Repository;
+using Core.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests.Services
+{
+    [TestClass()]
+    public class PricingServiceTests
+    {
+        private PricingService Service;
+        // mocks
+        private Mock<IRepository<Promotion>> _mockedPromoRepo;
+
+        [TestInitialize()]
+        public void Setup()
+        {
+            _mockedPromoRepo = new Mock<IRepository<Promotion>>();
+            Service = new PricingService(_mockedPromoRepo.Object);
+        }
+    }
+}
