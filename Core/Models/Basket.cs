8fe665712d03ed2c980fb651f7a772aabad89dd6
Lee Scholefield <leescholefield@gmail.com>
2020-12-06 13:49:05 +0000

    Add Basket class for keeping track of items the user has added, as well as its test class.

---
+++ Core/Models/Basket.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Core.Models
+{
+
+    /// <summary>
+    /// Represents a shopping basket for a single user.
+    /// 
+    /// In order to make accessing the items in the basket easier this class implements the Enumerable interface. This will return 
+    /// a <see cref="KeyValuePair{Item, int}"/> where the key is an Item in the basket and the value is the amount of items of that 
+    /// type in the basket.
+    /// </summary>
+    public class Basket : IEnumerable<KeyValuePair<Item, int>>
+    {
+
+        private readonly Dictionary<Item, int> _items = new Dictionary<Item, int>();
+
+        /// <summary>
+        /// Returns the total number of items in the basket. This includes multiples of the same item.
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return _items.Values.Sum();
+            }
+        }
+
+        /// <summary>
+        /// Returns the total amount of items matching <paramref name="item"/> in the basket.
+        /// </summary>
+        public int this[Item item]
+        {
+            get
+            {
+                return _items.GetValueOrDefault(item, 0);
+            }
+        }
+
+        /// <summary>
+        /// Returns the total amount of items where Item.SKU matches the given sku in the basket.
+        /// </summary>
+        public int this[string sku]
+        {
+            get
+            {
+                return _items.Where(x => x.Key.SKU.Equals(sku))
+                    .Select(x => x.Value)
+                    .FirstOrDefault();
+            }
+        }
+
+        /// <summary>
+        /// Adds the given amount of items to the basket.
+        /// </summary>
+        /// <param name="item">Item to add to the basket.</param>
+        /// <param name="amount">Number of copies to add.</param>
+        /// <exception cref="ArgumentNullException">If item is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">If amount is not greater than 0.</exception>
+        public void Add(Item item, int amount = 1)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("Item is null");
+            }
+
+            if (amount < 1)
+            {
+                throw new ArgumentOutOfRangeException("Invalid amount. Must be greater than 0");
+            }
+
+            if (_items.ContainsKey(item))
+            {
+                _items[item] += amount;
+            }
+            else
+            {
+                _items.Add(item, amount);
+            }
+        }
+
+        #region Enumerable Imp
+
+        // Since we already save the items in a dictionary we may as well return the enumerator from that instead of 
+        // having to implement our own.
+        public IEnumerator<KeyValuePair<Item, int>> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        // Must also implement IEnumerable.GetEnumerator, but implement as a private method.
+        private IEnumerator GetEnumerator1()
+        {
+            return GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator1();
+        }
+
+        #endregion
+    }
+}
---
+++ UnitTests/Models/BasketTest.cs
@@ -0,0 +1,56 @@
+﻿using Core.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests.Models
+{
+    [TestClass()]
+    public class BasketTests
+    {
+
+        private Basket basket;
+
+        [TestInitialize()]
+        public void Setup()
+        {
+            basket = new Basket();
+        }
+
+        [TestMethod()]
+        public void Add_Item()
+        {
+            basket.Add(new Item("A", 10));
+
+            Assert.AreEqual(1, basket["A"]);
+        }
+
+        [TestMethod()]
+        public void Add_Multiple_Of_Same_Item()
+        {
+            basket.Add(new Item("A", 10), 3);
+
+            Assert.AreEqual(3, basket["A"]);
+        }
+
+        [TestMethod()]
+        public void Count_Returns_Correct_Amount()
+        {
+            basket.Add(new Item("A", 10), 2);
+            basket.Add(new Item("B", 5), 3);
+
+            Assert.AreEqual(5, basket.Count);
+        }
+
+        [TestMethod()]
+        public void Adding_Null_Item_Throws_Exception()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => basket.Add(null));
+        }
+
+        [TestMethod()]
+        public void Adding_Minus_Amount_Throws_Exception()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => basket.Add(new Item("A", 10), -1));
+        }
+    }
+}
