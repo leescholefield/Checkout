5caba2269581362720333a13a19b8cf1bf0deffb
Lee Scholefield <leescholefield@gmail.com>
2020-12-06 14:02:00 +0000

    Add implementation of IPricingService.

---
+++ Core/Services/PricingService.cs
@@ -0,0 +1,24 @@
+﻿using Core.Models;
+using Core.Repository;
+using System;
+
+namespace Core.Services
+{
+    /// <inheritdoc cref="IPricingService"/>
+    public class PricingService : IPricingService
+    {
+
+        private readonly IRepository<Promotion> _promoRepository;
+
+
+        public PricingService(IRepository<Promotion> promoRepository)
+        {
+            _promoRepository = promoRepository;
+        }
+
+        public decimal CalculateBasketPrice(Basket basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
