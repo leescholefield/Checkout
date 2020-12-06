fd4d4abfe38e84d76e80835b5092e9be03338c29
Lee Scholefield <leescholefield@gmail.com>
2020-12-06 13:59:23 +0000

    Add Service interface for calculating the price of a basket.

---
+++ Core/Services/IPricingService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models;
+
+namespace Core.Services
+{
+    /// <summary>
+    /// Calcualtes the total price of a <see cref="Basket"/>.
+    /// </summary>
+    public interface IPricingService
+    {
+        /// <summary>
+        /// Returns the total price of all items in <paramref name="basket"/> after any applicable promotions have been applied.
+        /// </summary>
+        decimal CalculateBasketPrice(Basket basket);
+    }
+}
