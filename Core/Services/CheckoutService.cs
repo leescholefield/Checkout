9645ec2c9fb1d2891909397798f6e4575f1f8a07
Lee Scholefield <leescholefield@gmail.com>
2020-12-06 15:04:10 +0000

    Add implementation of ICheckoutService and tests

---
+++ Core/Services/CheckoutService.cs
@@ -0,0 +1,38 @@
+﻿using Core.Models;
+using System;
+
+namespace Core.Services
+{
+    /// <inheritdoc cref="ICheckoutService"/>
+    public class CheckoutService : ICheckoutService
+    {
+
+        private readonly IPricingService _pricingService;
+        private readonly Basket _basket;
+
+        public CheckoutService(IPricingService pricingService)
+        {
+            _pricingService = pricingService;
+            _basket = new Basket();
+        }
+
+        /// <inheritdoc/>
+        /// <exception cref="ArgumentNullException">If item is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">If count is not greater than 0.</exception>
+        public void Add(Item item, int count = 1)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("item is null");
+            }
+
+            _basket.Add(item, count);
+        }
+
+        /// <inheritdoc/>
+        public decimal GetTotal()
+        {
+            return _pricingService.CalculateBasketPrice(_basket);
+        }
+    }
+}
---
+++ UnitTests/Services/CheckoutServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Models;
+using Moq;
+
+namespace UnitTests.Services
+{
+    [TestClass()]
+    public class CheckoutServiceTests
+    {
+        private CheckoutService Service;
+
+        [TestInitialize()]
+        public void Setup()
+        {
+            Mock<IPricingService> _mockedPricingService = new Mock<IPricingService>();
+            Service = new CheckoutService(_mockedPricingService.Object);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Throws_Exception_When_Item_Is_Null()
+        {
+            Service.Add(null);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Throws_Exception_When_Count_Less_Than_0()
+        {
+            Service.Add(new Item("T", 1), -1);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Throws_Exception_When_Count_0()
+        {
+            Service.Add(new Item("T", 1), 0);
+        }
+    }
+}
